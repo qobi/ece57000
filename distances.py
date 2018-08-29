4629f69ab13f0f5fccdb6e2dc765abcc188cbf97
Jeffrey Mark Siskind <qobi@purdue.edu>
2018-08-29 01:31:58 -0400

    *** empty log message ***

--- distances.py
+++ distances.py
@@ -31,6 +31,39 @@ def dtw(distance):
         return c[m-1, n-1]
     return internal
 
+def dtw_with_image(distance):
+    def internal(s1, s2):
+        m = len(s1)
+        n = len(s2)
+        c = np.zeros((m, n))
+        b = [[False for j in range(0, n)] for i in range(0, m)]
+        image = np.ones((m, n))
+        c[0, 0] = distance(s1[0], s2[0])
+        for i in range(1, m):
+            c[i, 0] = distance(s1[i], s2[0])+c[i-1, 0]
+            b[i][0] = [i-1, 0]
+        for j in range(1, n):
+            c[0, j] = distance(s1[0], s2[j])+c[0, j-1]
+            b[0][j] = [0, j-1]
+        for i in range(1, m):
+            for j in range(1, n):
+                l = min(c[i-1, j], c[i, j-1], c[i-1, j-1])
+                c[i, j] = distance(s1[i], s2[j])+l
+                if l==c[i-1, j-1]:
+                    b[i][j] = [i-1, j-1]
+                elif l==c[i, j-1]:
+                    b[i][j] = [i, j-1]
+                elif l==c[i-1, j]:
+                    b[i][j] = [i-1, j]
+        i = m-1
+        j = n-1
+        while i>0 or j>0:
+            image[i, j] = 0
+            i, j = b[i][j]
+        image[i, j] = 0
+        return c[m-1, n-1], image
+    return internal
+
 infinity = float("inf")
 
 def hausdorf(distance):
